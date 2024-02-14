f72abd68d6378dff07fba6e00dbbaaefb8b11c70
Victor <victornuneshvh10@gmail.com>
2024-02-14 18:29:22 -0300

    :hammer: DB-init update

--- db-create/03.sql
+++ db-create/03.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS photos (
     id serial primary key,
-    url varchar(255) UNIQUE not null, 
+    url varchar(255) not null, 
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_name VARCHAR (50) not null references student_social_media (user_name) 
 );
